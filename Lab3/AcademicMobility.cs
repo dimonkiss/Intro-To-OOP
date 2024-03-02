782429d63e6b822e849265306150afa616cc09e1
dimonkiss <dklxwa@gmail.com>
2024-03-02 19:11:52 +0200

    add basic classes Student and AcademicMobility

---
+++ Lab3/AcademicMobility.cs
@@ -0,0 +1,114 @@
+﻿namespace Lab3
+{
+    public class AcademicMobility
+    {
+        private string[] international_programs;
+        private string[] university_partners;
+        private string[] contacts;
+
+        public AcademicMobility()
+        {
+            international_programs = new string[0];
+            university_partners = new string[0];
+            contacts = new string[0];
+        }
+
+        public AcademicMobility(string[] internationalPrograms, string[] universityPartners, string[] contacts)
+        {
+            this.international_programs = internationalPrograms;
+            this.university_partners = universityPartners;
+            this.contacts = contacts;
+        }
+
+        public string[] InternationalPrograms
+        {
+            get { return international_programs; }
+            set { international_programs = value; }
+        }
+
+        public string[] UniversityPartners
+        {
+            get { return university_partners; }
+            set { university_partners = value; }
+        }
+
+        public string[] Contacts
+        {
+            get { return contacts; }
+            set { contacts = value; }
+        }
+        
+         public void InputFromConsole()
+        {
+            Console.WriteLine("Enter Academic Mobility details:");
+            Console.Write("Number of International Programs: ");
+            int intProgramCount = int.Parse(Console.ReadLine());
+            international_programs = new string[intProgramCount];
+            for (int i = 0; i < intProgramCount; i++)
+            {
+                Console.Write($"Enter International Program {i + 1}: ");
+                international_programs[i] = Console.ReadLine();
+            }
+
+            Console.Write("Number of University Partners: ");
+            int uniPartnersCount = int.Parse(Console.ReadLine());
+            university_partners = new string[uniPartnersCount];
+            for (int i = 0; i < uniPartnersCount; i++)
+            {
+                Console.Write($"Enter University Partner {i + 1}: ");
+                university_partners[i] = Console.ReadLine();
+            }
+
+            Console.Write("Number of Contacts: ");
+            int contactsCount = int.Parse(Console.ReadLine());
+            contacts = new string[contactsCount];
+            for (int i = 0; i < contactsCount; i++)
+            {
+                Console.Write($"Enter Contact {i + 1}: ");
+                contacts[i] = Console.ReadLine();
+            }
+        }
+
+        public void OutputToConsole()
+        {
+            Console.WriteLine($"Academic Mobility Details:");
+            Console.WriteLine("International Programs:");
+            foreach (var program in international_programs)
+            {
+                Console.WriteLine(program);
+            }
+            Console.WriteLine("University Partners:");
+            foreach (var partner in university_partners)
+            {
+                Console.WriteLine(partner);
+            }
+            Console.WriteLine("Contacts:");
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine(contact);
+            }
+        }
+
+        public void WriteToFile(string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine("International Programs:");
+                foreach (var program in international_programs)
+                {
+                    writer.WriteLine(program);
+                }
+                writer.WriteLine("University Partners:");
+                foreach (var partner in university_partners)
+                {
+                    writer.WriteLine(partner);
+                }
+                writer.WriteLine("Contacts:");
+                foreach (var contact in contacts)
+                {
+                    writer.WriteLine(contact);
+                }
+            }
+        }
+    }
+}
---
+++ Lab3/Student.cs
@@ -0,0 +1,141 @@
+﻿namespace Lab3
+{
+    public class Student
+    {
+        private string surname;
+        private string name;
+        private int course;
+        private string speciality;
+        private string university;
+        private double rating;
+        private string[] science_achievements;
+
+        public Student()
+        {
+            surname = "";
+            name = "";
+            course = 0;
+            speciality = "";
+            university = "";
+            rating = 0.0;
+            science_achievements = new string[0];
+        }
+
+        public Student(string surname, string name, int course, string speciality, string university, double rating, string[] scienceAchievements)
+        {
+            this.surname = surname;
+            this.name = name;
+            this.course = course;
+            this.speciality = speciality;
+            this.university = university;
+            this.rating = rating;
+            this.science_achievements = scienceAchievements;
+        }
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Course
+        {
+            get { return course; }
+            set { course = value; }
+        }
+
+        public string Speciality
+        {
+            get { return speciality; }
+            set { speciality = value; }
+        }
+
+        public string University
+        {
+            get { return university; }
+            set { university = value; }
+        }
+
+        public double Rating
+        {
+            get { return rating; }
+            set { rating = value; }
+        }
+
+        public string[] ScienceAchievements
+        {
+            get { return science_achievements; }
+            set { science_achievements = value; }
+        }
+        public void InputFromConsole()
+        {
+            Console.WriteLine("Enter Student details:");
+            Console.Write("Surname: ");
+            surname = Console.ReadLine();
+
+            Console.Write("Name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Course: ");
+            course = int.Parse(Console.ReadLine());
+
+            Console.Write("Speciality: ");
+            speciality = Console.ReadLine();
+
+            Console.Write("University: ");
+            university = Console.ReadLine();
+
+            Console.Write("Rating: ");
+            rating = double.Parse(Console.ReadLine());
+
+            Console.Write("Number of science achievements: ");
+            int count = int.Parse(Console.ReadLine());
+            science_achievements = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"Enter achievement {i + 1}: ");
+                science_achievements[i] = Console.ReadLine();
+            }
+        }
+
+        public void OutputToConsole()
+        {
+            Console.WriteLine($"Student Details:");
+            Console.WriteLine($"Surname: {surname}");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Course: {course}");
+            Console.WriteLine($"Speciality: {speciality}");
+            Console.WriteLine($"University: {university}");
+            Console.WriteLine($"Rating: {rating}");
+            Console.WriteLine("Science Achievements:");
+            foreach (var achievement in science_achievements)
+            {
+                Console.WriteLine(achievement);
+            }
+        }
+
+        public void WriteToFile(string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine($"Surname: {surname}");
+                writer.WriteLine($"Name: {name}");
+                writer.WriteLine($"Course: {course}");
+                writer.WriteLine($"Speciality: {speciality}");
+                writer.WriteLine($"University: {university}");
+                writer.WriteLine($"Rating: {rating}");
+                writer.WriteLine("Science Achievements:");
+                foreach (var achievement in science_achievements)
+                {
+                    writer.WriteLine(achievement);
+                }
+            }
+        }
+    }
+}
