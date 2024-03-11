6570aa72048dd97f8a21243ed284cea25856f75d
dimonkiss <dklxwa@gmail.com>
2024-03-11 19:23:46 +0200

    add task 2-5

--- Lab4/Book.cs
+++ Lab4/Book.cs
@@ -4,4 +4,35 @@ public class Book
 {
     private string Name;
     private double Price;
+    
+    public Book()
+    {
+        // За замовчуванням присвоюємо пусті рядок для імені та 0.0 для ціни
+        Name = "";
+        Price = 0.0;
+    }
+    
+    public Book(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+    
+    public string NameProp
+    {
+        get { return Name; }
+        set { Name = value; }
+    }
+
+    public double PriceProp
+    {
+        get { return Price; }
+        set { Price = value; }
+    }
+    
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Price: $" + Price);
+    }
 }
--- Lab4/Compendium.cs
+++ Lab4/Compendium.cs
@@ -7,5 +7,38 @@ public class Compendium : Book
     private string StudentName;
     private string Language;
     private bool FreeAccess;
+    
+    
+    public Compendium(string name, double price, string studentName, string language, bool freeAccess) : base(name, price)
+    {
+        StudentName = studentName;
+        Language = language;
+        FreeAccess = freeAccess;
+    }
+    
+    public string StudentNameProp
+    {
+        get { return StudentName; }
+        set { StudentName = value; }
+    }
 
+    public string LanguageProp
+    {
+        get { return Language; }
+        set { Language = value; }
+    }
+
+    public bool FreeAccessProp
+    {
+        get { return FreeAccess; }
+        set { FreeAccess = value; }
+    }
+    
+    public new void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine("Student Name: " + StudentName);
+        Console.WriteLine("Language: " + Language);
+        Console.WriteLine("Free Access: " + FreeAccess);
+    }
 }
