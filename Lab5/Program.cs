3775dbd555617f2b0435584a262e2a26e483cce9
dimonkiss <dklxwa@gmail.com>
2024-03-16 00:15:00 +0200

    add task 7

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Mime;
 
 namespace Lab5
 {
@@ -6,7 +7,18 @@ namespace Lab5
     {
         static void Main(string[] args)
         {
-            
+            TextBook[] array = new TextBook[3];
+
+            // Ініціалізація об'єктів за допомогою індексатора
+            array[0] = new TextBook("Book1", 20.0, "Author1", 200, 4.5);
+            array[1] = new TextBook("Book2", 25.0, "Author2", 250, 4.8);
+            array[2] = new TextBook("Book3", 30.0, "Author3", 300, 4.9);
+
+            // Виведення значень полів об'єктів на консоль
+            foreach (var book in array)
+            {
+                book.DisplayInfo();
+            }
         }
     }
 }
