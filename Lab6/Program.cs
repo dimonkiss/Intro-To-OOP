d6b6efdabaf3a9c71e6dd93b73726990f1fbc409
dimonkiss <dklxwa@gmail.com>
2024-03-26 15:26:39 +0200

    add task 3

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Створення екземплярів необхідних компонентів для розумного кондиціонера
+            EmbeddedMicroprocessor microprocessor = new EmbeddedMicroprocessor();
+            ElectricalEquipment electricalEquipment = new ElectricalEquipment(new ElectricMotor(), new Compressor(), new Evaporator(), new Condenser(), new Thermostat(), new Refrigerant(), new AutomationDevices());
+            RefrigeratorCabinet refrigeratorCabinet = new RefrigeratorCabinet(new Housing(), new Door(), new ThermalInsulation());
 
-Console.WriteLine("Hello, World!");
+
+            // Створення розумного кондиціонера
+            SmartAirConditioner smartAC = new SmartAirConditioner(microprocessor, electricalEquipment, refrigeratorCabinet);
+
+            // Увімкнення розумного кондиціонера
+            smartAC.TurnOn();
+
+            // Встановлення температури
+            smartAC.SetTemperature(22.5);
+
+            // Вимкнення розумного кондиціонера
+            smartAC.TurnOff();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab6/SmertAirConditioner.cs
@@ -0,0 +1,32 @@
+﻿namespace Lab6;
+
+public class SmartAirConditioner : Refrigerator
+{
+    
+    public SmartAirConditioner(EmbeddedMicroprocessor microprocessor, ElectricalEquipment electricalEquipment, RefrigeratorCabinet refrigeratorCabinet)
+        : base(microprocessor, electricalEquipment, refrigeratorCabinet)
+    {
+        // Додаткові налаштування для кондиціонера
+    }
+
+    // Метод для ввімкнення кондиціонера
+    public void TurnOn()
+    {
+        Console.WriteLine("Розумний кондиціонер увімкнено.");
+        // Додаткові дії при ввімкненні
+    }
+
+    // Метод для вимкнення кондиціонера
+    public void TurnOff()
+    {
+        Console.WriteLine("Розумний кондиціонер вимкнено.");
+        // Додаткові дії при вимкненні
+    }
+
+    // Метод для регулювання температури кондиціонера
+    public void SetTemperature(double temperature)
+    {
+        Console.WriteLine($"Температура кондиціонера встановлена на {temperature} градусів Цельсія.");
+        // Додаткові дії при зміні температури
+    }
+}
